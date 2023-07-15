4aeed52f4a3367b40086414b4fe3d064c78d42b3
vassiliymarkov <88937545+vassiliymarkov@users.noreply.github.com>
2023-07-15 13:18:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,66 +4,28 @@
 let wrapper = document.querySelector('.wrapper');
 
 let pageSlider = new Swiper('.page', {
-	// Свои классы
 	wrapperClass: "page__wrapper",
 	slideClass: "page__screen",
-
-	// Вертикальный слайдер
 	direction: 'vertical',
-
-	// Количество слайдов для показа
 	slidesPerView: 'auto',
-
-	// Включаем параллакс
 	parallax: true,
 
-
-	// Управление клавиатурой
 	keyboard: {
-		// Включить\выключить
 		enabled: true,
-		// Включить\выключить
-		// только когда слайдер
-		// в пределах вьюпорта
 		onlyInViewport: true,
-		// Включить\выключить
-		// управление клавишами
-		// pageUp, pageDown
 		pageUpDown: true,
 	},
 
-	// Управление колесом мыши
 	mousewheel: {
-		// Чувствительность колеса мыши
 		sensitivity: 1,
-		// Класс объекта на котором
-		// будет срабатывать прокрутка мышью.
-		//eventsTarget: ".image-slider"
 	},
 
-	// Отключение функционала
-	// если слайдов меньше чем нужно
 	watchOverflow: true,
 
-	// Скорость
 	speed: 800,
-
-	// Обновить свайпер
-	// при изменении элементов слайдера
 	observer: true,
-
-	// Обновить свайпер
-	// при изменении родительских
-	// элементов слайдера
 	observeParents: true,
-
-	// Обновить свайпер
-	// при изменении дочерних
-	// элементов слайда
 	observeSlideChildren: true,
-
-	// Навигация 
-	// Буллеты, текущее положение, прогрессбар
 	pagination: {
 		el: '.page__pagination',
 		type: 'bullets',
@@ -71,26 +33,20 @@ let pageSlider = new Swiper('.page', {
 		bulletClass: "page__bullet",
 		bulletActiveClass: "page__bullet_active",
 	},
-	// Скролл
-	scrollbar: {
+
+    scrollbar: {
 		el: '.page__scroll',
 		dragClass: "page__drag-scroll",
-		// Возможность перетаскивать скролл
 		draggable: true
 	},
 
-	// Отключаем автоинициализацию
 	init: false,
-
-	// События
 	on: {
-		// Событие инициализации
 		init: function () {
 			menuSlider();
 			setScrollType();
 			wrapper.classList.add('_loaded');
 		},
-		// Событие смены слайда
 		slideChange: function () {
 			menuSliderRemove();
 			menuLinks[pageSlider.realIndex].classList.add('_active');
